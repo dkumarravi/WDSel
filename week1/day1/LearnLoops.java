1a145ed222091ceff0962899c3983be77319a247
TestLeafPages <sarath.muthuswamy@testleaf.com>
2019-06-03 11:51:22 +0530

    Add files via upload

---
+++ week1/day1/LearnLoops.java
@@ -0,0 +1,41 @@
+package week1.day1;
+
+public class LearnLoops {
+	public static void main(String[] args) {
+		/*int num =90;
+		if(num==10) {
+			System.out.println("Ten");
+		}else if(num==9)
+		{
+			System.out.println("Nine");
+		}
+		else
+		{
+			System.out.println("! Ten");
+		}*/
+
+		for(int i=0 ; i <= 10 ; i++) {
+			System.out.println("2 * "+i+ " = "+2*i);
+			/*if (i % 2 != 0) {
+				System.out.println("ODD number is "+i);
+			}*/
+		}
+		/*	int age =8;
+		boolean isAgeAbove18 = true;
+		System.out.println(isAgeAbove18);
+		if(age>=18) {
+			isAgeAbove18 = true;
+		}
+		else {
+			isAgeAbove18 = false;
+		}
+		System.out.println(isAgeAbove18);
+		 */
+		/*int num =5;
+		int i =0;
+		while(i<=num) {
+			System.out.println(i);
+			i++;
+		}*/
+	}
+}
