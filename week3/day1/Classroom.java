0728fc71a39f19e45b1829ec2cf5f7253dfdd942
TestLeafPages <sarath.muthuswamy@testleaf.com>
2019-06-17 11:57:07 +0530

    Testng & String

---
+++ week3/day1/Classroom.java
@@ -0,0 +1,61 @@
+package week3.day1;
+
+public class Classroom {
+public static void main(String[] args) {
+	String str = "Testleaf";
+	StringBuffer str1 = new StringBuffer("Testleaf");
+	str1.append("Chennai");
+	System.out.println(str1);
+	System.out.println(str1.reverse());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*String str1 = "Testleaf";
+	String txt = new String("Testleaf");
+	System.out.println(txt==str);
+	System.out.println(txt.equals(str));
+	String upperCase = str.toUpperCase();
+	System.out.println(str.toUpperCase());
+	System.out.println(str);
+	System.out.println(str1==str);*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*String temp = "";
+	int count=0;
+	char[] array = str.toCharArray();
+	for (int i = str.length()-1; i >=0; i--) {
+		System.out.print(str.charAt(i));
+//		System.out.println(array[i]);
+		if(array[i]=='e') {
+			count++;
+		}
+//		temp+=array[i];
+	System.out.println(temp);
+	System.out.println(count);
+	}*/
+	
+}
+}
---
+++ week3/day1/LearnString.java
@@ -0,0 +1,48 @@
+package week3.day1;
+
+public class LearnString {
+
+	public static void main(String[] args) {
+		String findSub = "TestLeaf 1234";
+		System.out.println(findSub.replaceAll("\\d", ""));
+		/*
+		System.out.println(findSub.replace("es", " "));
+		
+		System.out.println(findSub.substring(4));*/
+		
+		
+		String sen = "im good boy";
+		/*String[] split = sen.split(" ");
+		System.out.println(split[0]);
+		for (String string : split) {
+			System.out.println(string);
+		}*/
+		
+		
+		/*String st = "  i have spaces  ";
+		System.out.println(st.trim());
+		String name = "koushik";
+		char[] charArray = name.toCharArray();
+		for (char c : charArray) {
+			System.out.println(c);
+		}
+		*/
+		
+		
+		
+		
+	/*	String str = "i am in testleaf";
+		int length = str.length();
+		System.out.println(length);
+		String st = "I am IN testleaf";
+		System.out.println(st.toLowerCase());
+		System.out.println(str.equalsIgnoreCase(st));*/
+		/*boolean contains = str.contains(st);
+		System.out.println(contains);*/
+		/*boolean equals = str.equals(st);
+		System.out.println(equals);*/
+		
+
+	}
+
+}
