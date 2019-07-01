695b4e68f6d76506ed4e2cbe1e19dd4d444455eb
TestLeafPages <sarath.muthuswamy@testleaf.com>
2019-07-01 11:56:32 +0530

    @FindBy

--- pages/LoginPage.java
+++ pages/LoginPage.java
@@ -1,23 +1,42 @@
 package com.testleaf.leaftaps.pages;
 
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 import com.autoBot.testng.api.base.Annotations;
 
 public class LoginPage extends Annotations{
 	
-	public LoginPage() {}
-
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	private List<WebElement> us;
+	@CacheLookup
+	@FindBy(how = How.ID, using = "username")
+	private WebElement eleUserName;
+	
+	@FindBy(how = How.ID, using = "password")
+	private WebElement elePassword;
+	
+	@FindBy(how = How.CLASS_NAME, using = "decorativeSubmit")
+	private WebElement eleLoginButton;
+	
 	public LoginPage enterUserName(String un) {
-		clearAndType(locateElement("id", "username"), 
-				un);
+		clearAndType(eleUserName, un);
 		return this;
 	}
 	public LoginPage enterPassword(String pwd) {
-		clearAndType(locateElement("id", "password"), pwd);
+		clearAndType(elePassword, pwd);
 		return this;
 	}
-	
 	public HomePage clickLogin() {
-		click(locateElement("class", "decorativeSubmit"));
+		click(eleLoginButton);
 		return new HomePage();
 	}
 	
